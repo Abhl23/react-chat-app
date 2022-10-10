a5cdbd4a92f0d6bd16e742c1388bcd5e18ba77b6
Abhi <bhatt23abhishek@gmail.com>
2022-10-10 19:29:19 +0530

    create Signup page

---
+++ src/pages/Signup.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { Link, useNavigate } from "react-router-dom";
+
+import { auth, storage, db } from "../firebase";
+import { useChat, useFormInput } from "../hooks";
+import styles from "../styles/signup.module.scss";
+
+const Signup = () => {
+  const username = useFormInput("");
+  const email = useFormInput("");
+  const password = useFormInput("");
+
+  const [avatar, setAvatar] = useState(null);
+
+  const navigate = useNavigate();
+
+  const { dispatch } = useChat();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(username.value, email.value, password.value, avatar);
+
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email.value,
+        password.value
+      );
+
+      const storageRef = await ref(storage, username.value);
+
+      const uploadTask = uploadBytesResumable(storageRef, avatar);
+
+      uploadTask.on(
+        (error) => {
+          console.log("Error", error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(response.user, {
+              displayName: username.value,
+              photoURL: downloadURL,
+            });
+
+            await setDoc(doc(db, "users", response.user.uid), {
+              uid: response.user.uid,
+              displayName: username.value,
+              email: email.value,
+              photoURL: downloadURL,
+            });
+
+            await setDoc(doc(db, "userConversations", response.user.uid), {});
+
+            navigate("/");
+          });
+        }
+      );
+    } catch (error) {
+      console.log("Error:", error);
+    }
+
+    dispatch({
+      type: "RESET_USER",
+    });
+  };
+
+  return (
+    <div className={styles.formContainer}>
+      <div className={styles.formWrapper}>
+        <span className={styles.logo}>React Chat</span>
+        <span className={styles.heading}>Sign Up</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="username" {...username} />
+          <input type="email" placeholder="email" {...email} />
+          <input type="password" placeholder="password" {...password} />
+          <input
+            style={{ display: "none" }}
+            type="file"
+            id="avatar"
+            onChange={(e) => setAvatar(e.target.files[0])}
+          />
+          <label htmlFor="avatar">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/5460/5460486.png"
+              alt="add-avatar"
+            />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>
+          Already have an account? <Link to="/login">Log In</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
