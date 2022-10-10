b6b24d9902ce8ba3a8a22a7ac5ab81e44dda5a77
Abhi <bhatt23abhishek@gmail.com>
2022-10-10 19:34:04 +0530

    create Texts component

---
+++ src/components/Texts.js
@@ -0,0 +1,37 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+import { db } from "../firebase";
+import { useChat } from "../hooks";
+import styles from "../styles/chat.module.scss";
+import Text from "./Text";
+
+const Texts = () => {
+  const [messages, setMessages] = useState([]);
+
+  const { data } = useChat();
+
+  useEffect(() => {
+    const getMessages = () => {
+        const unsub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+            doc.exists() && setMessages(doc.data().messages);
+          });
+      
+          return () => {
+            unsub();
+          };
+    };
+
+    data.chatId && getMessages();
+  }, [data.chatId]);
+
+  return (
+    <div className={styles.messages}>
+      {messages.map((message) => (
+        <Text message={message} key={message.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Texts;
